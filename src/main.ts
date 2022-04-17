c855085bb589d5c1483c2c5324fe8c5ccfb2a18f
HassanSaeed7 <Hsaeed1007@gmail.com>
2022-04-16 20:49:08 -0700

    g

--- src/main.ts
+++ src/main.ts
@@ -8,53 +8,66 @@ import * as THREE from 'three';
 
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 
   renderer.setSize( window.innerWidth, window.innerHeight );
   renderer.setPixelRatio(devicePixelRatio);  
   document.body.appendChild( renderer.domElement );
 
-  const ball = new THREE.TextureLoader().load('./textures/ball2.jpg')
+  const ball = new THREE.TextureLoader().load('/textures/1.jpg')
 
   //light
-  const dirLight = new THREE.DirectionalLight(0xff0000, 0.95)
-  dirLight.position.set(0, 0, 1)
-  const dirLight1 = new THREE.DirectionalLight(0xff0000, 0.95)
-  dirLight1.position.set(0, 0, -1)
-  const ambLight = new THREE.AmbientLight(0xff0000); // soft white light
-  const pointLight = new THREE.PointLight(0xff0000, 1);
-  pointLight.position.set(30, 30, 60);
 
-  const spotLight = new THREE.SpotLight(0xffffff);
-  spotLight.position.set(100, 2000, 90);
-  spotLight.castShadow = true;
-  spotLight.shadow.mapSize.width = 1024;
-  spotLight.shadow.mapSize.height = 1024;
-  spotLight.shadow.camera.near = 100;
-  spotLight.shadow.camera.far = 1000;
-  spotLight.shadow.camera.fov = 100;
+  const pointLight = new THREE.PointLight(0xff0000, 30);
+  pointLight.position.set(400, 1060, 10);
+  
 
-	const geometry = new THREE.SphereGeometry();
-  const material = new THREE.MeshStandardMaterial({
+	const geometry = new THREE.SphereGeometry(1, 100, 10);
+
+
+  // const material = new THREE.MeshPhysicalMaterial({
+  //   map: ball,
+  //   reflectivity: 0.78,
+  //   roughness: 0,
+  //   metalness: 1,
+  //   clearcoat: 0.4,
+  //   clearcoatRoughness: 0.25,
+  //   transmission: 1,
+ // });
+
+  const material = new THREE.MeshPhongMaterial({
     map: ball,
-    metalness: 0.4,
+    shininess: 0.6,
+   
   });
+
+//   const material = new THREE.MeshPhysicalMaterial({})
+// material.reflectivity = 0
+// material.transmission = 1.0
+// material.roughness = 0
+// material.metalness = 0
+// material.clearcoat = 0.6
+// material.clearcoatRoughness = 0.4
+// material.map = ball
+// material.ior = 1
+// material.thickness = 10.0
+
+
 	const cube = new THREE.Mesh( geometry, material );
 
 
 
 
-
-	camera.position.z = 1.2;
+	camera.position.z = 2;
   camera.position.y = 0.5;		
 
-  scene.add( cube, spotLight );
+  scene.add( cube, pointLight );
        
 function animate() {
 		requestAnimationFrame( animate );
-			cube.rotation.x -= 0.005;
-			// cube.rotation.y += 0.01;
+			cube.rotation.x -= 0.01;
+			// cube.rotation.y += 0.005;
 			renderer.render( scene, camera );
 };
 
